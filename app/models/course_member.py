7d5ce0762f44b093c95b0d2583db58a2c24d8ada
Opey Adeyemi <60050089+Opeyem1a@users.noreply.github.com>
2024-02-24 18:22:49 -0700

    add model definitions (#14)
    * add model definitions
    
    * clean up migrations and FK to MyUser

--- app/models/course_member.py
+++ app/models/course_member.py
@@ -1,23 +1,20 @@
-from django.contrib.auth.models import User
 from django.db import models
 
+from accounts.models import MyUser
 from app.models.base_models import BaseModel
-from django.utils.translation import gettext_lazy as _
-
 from app.models.course import Course
 from app.models.section import Section
 
 
 class UserRole(models.TextChoices):
-    STUDENT = "S", _("Student")
-    INSTRUCTOR = "I", _("Instructor")
+    STUDENT = "Student", "Student"
+    INSTRUCTOR = "Instructor", "Instructor"
 
 
 class CourseMember(BaseModel):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="course_memberships"
+        MyUser, on_delete=models.CASCADE, related_name="course_memberships"
     )
     sections = models.ManyToManyField(Section, related_name="section_members")
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    role = models.CharField(max_length=1, choices=UserRole.choices)
-    external_lms_id = models.CharField(max_length=100, blank=True)
+    role = models.CharField(max_length=50, choices=UserRole.choices)
---
+++ app/models/team.py
@@ -0,0 +1,61 @@
+from app.models.attribute import Attribute, AttributeOption
+from app.models.base_models import BaseModel
+from django.db import models
+
+from app.models.course_member import CourseMember
+
+
+class RequirementSubject(models.TextChoices):
+    EVERYONE = "Everyone", "Everyone"
+    SOMEONE = "Someone", "Someone"
+
+
+class RequirementOperator(models.TextChoices):
+    # todo: "OVERLAPS" and "NOT_OVERLAPS"
+    GT = "GT", "Greater Than"
+    GTE = "GTE", "Greater Than or Equal"
+    LT = "LT", "Less Than"
+    LTE = "LTE", "Less Than or Equal"
+    IN = "IN", "In"
+    NOT_IN = "NOT_IN", "Not In"
+    CONTAINS = "CONTAINS", "Contains"
+    EQ = "EQ", "Equal"
+
+
+class TeamSet(BaseModel):
+    name = models.CharField(max_length=250)
+
+
+class Team(BaseModel):
+    slug = models.UUIDField()
+    max_people = models.IntegerField()
+    min_people = models.IntegerField()
+    members = models.ManyToManyField(CourseMember, related_name="teams")
+
+
+class TeamRequirement(BaseModel):
+    team = models.ForeignKey(
+        Team, on_delete=models.CASCADE, related_name="requirements"
+    )
+
+
+class TeamSetTemplate(BaseModel):
+    name = models.CharField(max_length=250)
+
+
+class TeamTemplate(BaseModel):
+    description = models.TextField()
+    slug = models.UUIDField()
+    number_of_teams = models.IntegerField()
+    max_people = models.IntegerField()
+    min_people = models.IntegerField()
+
+
+class TeamTemplateRequirement(BaseModel):
+    team_template = models.ForeignKey(
+        TeamTemplate, on_delete=models.CASCADE, related_name="requirements"
+    )
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+    operator = models.CharField(max_length=50, choices=RequirementOperator.choices)
+    subject = models.CharField(max_length=20, choices=RequirementSubject.choices)
+    options = models.ManyToManyField(AttributeOption)
