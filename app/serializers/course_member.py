1bbcf731a03653434f035a381c602a43b4b9851d
justino599 <justin.schoenit@gmail.com>
2024-02-29 18:10:29 -0800

    Add foreign key references to CourseMember model

--- app/serializers/course_member.py
+++ app/serializers/course_member.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from app.models.course_member import CourseMember
+from app.serializers.attribute import AttributeResponseSerializer
+from app.serializers.relationship import RelationshipSerializer
 
 
 class CourseMemberSerializer(serializers.ModelSerializer):
+    outbound_relationships = RelationshipSerializer(
+        "outbound_relationships", many=True, read_only=True
+    )
+    attribute_responses = AttributeResponseSerializer(
+        "attribute_responses", many=True, read_only=True
+    )
+
     class Meta:
         model = CourseMember
         fields = "__all__"
