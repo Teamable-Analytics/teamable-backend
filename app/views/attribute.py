12e14ea73b86834469c73cf7e707e12525463352
Sanabob <minhquanle_2002@yahoo.com>
2024-03-09 00:01:30 -0800

    removed custom filter for premission

--- app/views/attribute.py
+++ app/views/attribute.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from django_filters import rest_framework as filters
 
-from app.filters.attribute import FilterAttributes
 from app.models import Attribute
 from app.serializers.attribute import AttributeSerializer
 
@@ -9,5 +8,5 @@ from app.serializers.attribute import AttributeSerializer
 class AttributeViewSet(viewsets.ModelViewSet):
     queryset = Attribute.objects.all()
     serializer_class = AttributeSerializer
-    filter_backends = [filters.DjangoFilterBackend, FilterAttributes]
+    filter_backends = [filters.DjangoFilterBackend]
     filterset_fields = ["course_id"]
