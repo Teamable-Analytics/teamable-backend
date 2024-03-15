046c0d650c6a1d947fedacbab35eb0c0887f494b
opeyem1a <opeyadeyemi@gmail.com>
2024-03-15 08:12:22 -0600

    add Organization model

--- app/models/course.py
+++ app/models/course.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 from app.models.base_models import BaseModel
+from app.models.organization import Organization
 
 
 class Course(BaseModel):
     name = models.CharField(max_length=500)
+    organization = models.ForeignKey(Organization, null=True, blanlk=True, on_delete=models.SET_NULL)
