72fb7ad38c0cdda2fa1b18fad7df11f423a8d39b
Abhinav Thota <abhinavthota01@gmail.com>
2024-03-17 12:04:06 -0700

    Get course_member filter working on course_member/course/[course] endpoint

--- app/views/course_member.py
+++ app/views/course_member.py
@@ -7,21 +7,27 @@ from app.serializers.course_member import CourseMemberSerializer
 
 
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class CourseMemberViewSet(viewsets.ModelViewSet):
     queryset = CourseMember.objects.all()
     serializer_class = CourseMemberSerializer
+    pagination_class = ExamplePagination
+    ordering_fields = "__all__"
+    filter_backends = [FilterStudents]
 
     @action(detail=False, methods=["get"], url_path="course/(?P<course>[^/.]+)")
     def by_course(self, request, course):
         queryset = CourseMember.objects.filter(course=course)
-        serializer = self.get_serializer(queryset, many=True)
+
+        for backend in self.filter_backends:
+            queryset = backend().filter_queryset(request, queryset, self)
+
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
-
-    pagination_class = ExamplePagination
-    ordering_fields = "__all__"
-    filter_backends = [FilterStudents]
+        else:
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
