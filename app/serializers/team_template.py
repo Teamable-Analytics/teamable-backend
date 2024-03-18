f9571ab8148bdc864583a4a6b06fb557f544635e
ketphan02 <tuankietvn@outlook.com>
2024-03-17 19:52:48 -0700

    Serializer + ViewSet

--- app/serializers/team_template.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from app.models import TeamTemplate
-
-
-class TeamTemplateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TeamTemplate
-        fields = '__all__'
--- app/urls.py
+++ app/urls.py
@@ -5,12 +5,26 @@ from app.views.course import CourseViewSet
 from app.views.course_member import CourseMemberViewSet
 from app.views.relationship import RelationshipViewSet
 from app.views.section import SectionViewSet
+from app.views.team import (
+    TeamTemplateViewSet,
+    TeamViewSet,
+    TeamSetViewSet,
+    TeamRequirementViewSet,
+    TeamSetTemplateViewSet,
+    TeamTemplateRequirementViewSet
+)
 
 router = routers.DefaultRouter()
 router.register("courses", CourseViewSet)
 router.register("sections", SectionViewSet)
 router.register("course-members", CourseMemberViewSet)
 router.register("relationships", RelationshipViewSet)
+router.register("team-set", TeamSetViewSet)
+router.register("team", TeamViewSet)
+router.register("team-requirement", TeamRequirementViewSet)
+router.register("team-set-template", TeamSetTemplateViewSet)
+router.register("team_templates", TeamTemplateViewSet)
+router.register("team-template-requirements", TeamTemplateRequirementViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
