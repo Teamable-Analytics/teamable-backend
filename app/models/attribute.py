cfde2e9dddc0fa4bf318b3730d05e5efb3d93c38
Sanabob <minhquanle_2002@yahoo.com>
2024-03-29 23:03:58 -0700

    temp

--- app/models/attribute.py
+++ app/models/attribute.py
@@ -31,12 +31,18 @@ class Attribute(BaseModel):
         return AttributeResponse.objects.filter(
             attribute_option__attribute=self
         ).exists()
+        # return self.num_student_responses > 0
 
     @property
     def num_student_responses(self):
-        return AttributeResponse.objects.filter(
-            attribute_option__attribute=self
-        ).count()
+        # return AttributeResponse.objects.filter(
+        #     attribute_option__attribute=self
+        # ).count()
+        print(self.options)
+        return "self.options"
+        # return self.attribute_options.aggregate(
+        #     num_student_responses=models.Count("attribute_responses")
+        # )["num_student_responses"]
 
 
 class AttributeOption(BaseModel):
@@ -52,7 +58,8 @@ class AttributeOption(BaseModel):
 
     @property
     def num_student_responses(self):
-        return AttributeResponse.objects.filter(attribute_option=self).count()
+        # return AttributeResponse.objects.filter(attribute_option=self).count()
+        return self.attribute
 
 
 class AttributeResponse(BaseModel):
@@ -62,3 +69,10 @@ class AttributeResponse(BaseModel):
     attribute_option = models.ForeignKey(
         AttributeOption, on_delete=models.CASCADE, related_name="attribute_responses"
     )
+
+    @staticmethod
+    def clear_attribute_responses(attribute_id):
+        num_attribute_responses_deleted, _ = AttributeResponse.objects.filter(
+            attribute_option_id=attribute_id
+        ).delete()
+        return num_attribute_responses_deleted
--- app/views/attribute.py
+++ app/views/attribute.py
@@ -16,26 +16,26 @@ class AttributeViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.DjangoFilterBackend]
     filterset_fields = ["course_id"]
 
-    @staticmethod
-    def clear_attribute_responses(attribute_id):
-        num_attribute_responses_deleted, _ = AttributeResponse.objects.filter(
-            attribute_option_id=attribute_id
-        ).delete()
-        return num_attribute_responses_deleted
-
     @action(detail=False, methods=["post"])
     def delete_student_responses(self, request):
-        if "attribute_option_id" in request.data:
-            if get_object_or_404(
-                AttributeOption, pk=request.data["attribute_option_id"]
-            ).has_student_responses:
-                num_attribute_responses_deleted = self.clear_attribute_responses(
-                    request.data["attribute_option_id"]
-                )
-            else:
-                raise ObjectDoesNotExist(
-                    "No student responses found for this attribute option"
-                )
-        else:
-            raise FieldError("attribute_option_id field is required")
-        return Response(f"{num_attribute_responses_deleted} responses deleted")
+        if "attribute_id" in request.data:
+            attribute = get_object_or_404(Attribute, pk=request.data["attribute_id"])
+            print(AttributeOption.objects.all().values_list("attribute_id", flat=True))
+            return Response(attribute.num_student_responses)
+        return Response("Hello, World!")
+    
+    # @action(detail=False, methods=["post"])
+    # def delete_student_responses(self, request):
+    #     if "attribute_option_id" in request.data:
+    #         attribute_option = get_object_or_404(AttributeOption, pk=request.data["attribute_option_id"])
+    #         if attribute_option.has_student_responses:
+    #             num_attribute_responses_deleted = AttributeResponse.clear_attribute_responses(
+    #                 request.data["attribute_option_id"]
+    #             )
+    #         else:
+    #             raise ObjectDoesNotExist(
+    #                 "No student responses found for this attribute option"
+    #             )
+    #     else:
+    #         raise FieldError("attribute_option_id field is required")
+    #     return Response({num_attribute_responses_deleted: num_attribute_responses_deleted, attribute_id: attribute_option_id})
