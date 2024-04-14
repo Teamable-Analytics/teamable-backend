e08f477bb67af91c70cefebd6399ab1774866918
Abhinav Thota <abhinavthota01@gmail.com>
2024-04-13 22:34:33 -0700

    remove dead import

--- app/views/course_member.py
+++ app/views/course_member.py
@@ -13,7 +13,6 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status
 
-from django.shortcuts import get_object_or_404
 from django.db import transaction
 
 
