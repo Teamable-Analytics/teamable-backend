b2395bf47d7ed266bd4c382300c1288bf36d5cac
Sanabob <minhquanle_2002@yahoo.com>
2024-04-28 19:29:36 -0700

    fix workflow + fixed comments

--- app/serializers/course.py
+++ app/serializers/course.py
@@ -1,18 +1,9 @@
 from rest_framework import serializers
 
 from app.models.course import Course
-from app.serializers.attribute import AttributeSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    attributes = AttributeSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-
-class CustomCourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ["id", "name"]
--- app/views/course.py
+++ app/views/course.py
@@ -1,25 +1,9 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
-from django_filters import rest_framework as filters
 from app.models.course import Course
-from app.serializers.course import CourseSerializer, CustomCourseSerializer
+from app.serializers.course import CourseSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
-    filter_backends = [filters.DjangoFilterBackend]
-    filterset_fields = ["id"]
-
-    def get_queryset(self):
-        course_id = self.request.query_params.get("id", None)
-        if course_id:
-            get_object_or_404(Course, pk=course_id)
-            return self.queryset
-        return self.queryset
-
-    def get_serializer_class(self):
-        course_id = self.request.query_params.get("id", None)
-        if course_id:
-            return CustomCourseSerializer
-        return CourseSerializer
+    serializer_class = CourseSerializer
