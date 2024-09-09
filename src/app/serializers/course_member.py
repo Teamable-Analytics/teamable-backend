7f52a3ef1b8a46bb43ec2e5712f4f2acb3604c2b
Opey Adeyemi <60050089+Opeyem1a@users.noreply.github.com>
2024-09-08 22:25:02 -0600

    lms link for students (#52)

--- src/app/serializers/course_member.py
+++ src/app/serializers/course_member.py
@@ -1,29 +1,23 @@
 from rest_framework import serializers
 
 from app.models.course_member import CourseMember
-from app.serializers.attribute import AttributeResponseSerializer
-from app.serializers.relationship import RelationshipSerializer
 from app.serializers.section import SectionSerializer
 
 
 class CourseMemberSerializer(serializers.ModelSerializer):
-    outbound_relationships = RelationshipSerializer(
-        "outbound_relationships", many=True, read_only=True
-    )
-    attribute_responses = AttributeResponseSerializer(
-        "attribute_responses", many=True, read_only=True
-    )
     sections = SectionSerializer(many=True, read_only=True)
 
     class Meta:
         model = CourseMember
-        fields = "__all__"
+        fields = ("id", "lms_id", "name", "role", "lms_link", "sections")
 
 
 class DisplayCourseMemberSerializer(serializers.ModelSerializer):
+    lms_link = serializers.CharField(read_only=True)
+
     class Meta:
         model = CourseMember
-        fields = ("id", "lms_id", "name", "role")
+        fields = ("id", "lms_id", "name", "role", "lms_link")
 
 
 class CourseMemberListSerializer(serializers.ModelSerializer):
