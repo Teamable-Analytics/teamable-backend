aeec15a3b7826e15645df716b1a0a0d6799f726d
Opey Adeyemi <60050089+Opeyem1a@users.noreply.github.com>
2024-09-10 17:48:23 -0600

    09 08 uniquely name exported team sets in canvas (#54)
    * uniquely name exported team sets in Canvas
    
    * add max retries
    
    * remove dead import

--- src/app/canvas/export_team.py
+++ src/app/canvas/export_team.py
@@ -1,4 +1,6 @@
-from canvasapi import Canvas
+from canvasapi import Canvas, exceptions
+from canvasapi.course import Course
+from canvasapi.group import GroupCategory
 
 from app.models.course_member import CourseMember
 from app.models.organization import LMSTypeOptions
@@ -16,10 +18,12 @@ def export_team_to_canvas(team_set: TeamSet):
     canvas = Canvas(course.organization.lms_api_url, course.lms_access_token)
     canvas_course = canvas.get_course(course.lms_course_id)
 
-    group_category = canvas_course.create_group_category(name=team_set.name)
+    group_category = create_group_category_with_unique_name(
+        canvas_course=canvas_course, base_name=team_set.name
+    )
 
-    for i, team in enumerate(team_set.teams.all()):
-        group = group_category.create_group(name=i)
+    for team in team_set.teams.all():
+        group = group_category.create_group(name=team.name)
 
         for member in team.members.all():
             member: CourseMember
@@ -27,3 +31,26 @@ def export_team_to_canvas(team_set: TeamSet):
                 # fixme: this feels bad
                 continue
             group.create_membership(int(member.lms_id))
+
+
+# arbitrary limit so this does not run infinitely if the error is not the "same name" error
+MAX_RETIRES = 20
+
+
+def create_group_category_with_unique_name(
+    canvas_course: Course, base_name: str
+) -> GroupCategory:
+    group_category = None
+    counter = 1
+
+    while group_category is None:
+        try:
+            name = f"{base_name} ({counter})" if counter > 1 else f"{base_name}"
+            group_category = canvas_course.create_group_category(name=name)
+        except exceptions.BadRequest as e:
+            # fixme: we should probably be more specific about this error
+            counter += 1
+            if counter > MAX_RETIRES:
+                raise e
+
+    return group_category
