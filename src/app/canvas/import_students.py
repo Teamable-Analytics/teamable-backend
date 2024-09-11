e4ae3c87698f2bfaa58b59b67a20dfb7bac267e6
Opey Adeyemi <60050089+Opeyem1a@users.noreply.github.com>
2024-09-10 21:57:09 -0600

    add sis_user_id (#55)
    * add sis_user_id
    
    * lint

--- src/app/canvas/import_students.py
+++ src/app/canvas/import_students.py
@@ -36,10 +36,12 @@ def import_students_from_canvas(course: Course):
         ]
 
     for student in students:
+        # todo: make this upsert, not get and create
         CourseMember.add_course_member(
             user_id=None,
             name=student.user["name"],
             lms_id=student.user["id"],
+            sis_user_id=student.sis_user_id,
             course_id=course.pk,
             role=UserRole.STUDENT,
         )
